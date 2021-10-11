ebd03bcbf402e76071f1e7b3962d07876218ddee
HappyFood81 <86101694+HappyFood81@users.noreply.github.com>
2021-10-11 12:14:25 +0800

    Update merchantAPI.js

--- routes/merchantAPI.js
+++ routes/merchantAPI.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 const Post = require('../model/MERCHANT');
 
+
+router.get('/', async (req, res) => {
+    try {
+        const post = await Post.find();
+        if (!post) throw Error("No items found");
+
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(400).json({ msg: err });
+    }
+});
+
 router.get(':/id', async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
