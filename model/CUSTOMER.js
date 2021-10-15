f385190c0eb866db33f0b3d2e29eff3097f98b0f
HappyFood81 <86101694+HappyFood81@users.noreply.github.com>
2021-10-15 23:07:49 +0800

    Update CUSTOMER.js

--- model/CUSTOMER.js
+++ model/CUSTOMER.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-    resto: { type: String, required: true },
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    house: { type: String, required: true },
-    email: { type: String, required: true },
+    resto: { type: String, required: true},
+    name: { type: String, required: true},
+    phone: { type: String, required: true},
+    house: { type: String, required: true},
+    email: { type: String, required: true},
     foodNumber: { type: String, required: true },
-    subTotal: { type: String, required: true },
-    delFee: { type: String, required: true },
-    total: { type: String, required: true },
-    time: { type: String, required: true },
-    orders: { type: Array, required: true }
+    subTotal: { type: String, required: true},
+    delFee: { type: String, required: true},
+    total: { type: String, required: true},
+    time: { type: String, required: true},
+    orders: { type: Array, required: true},
+    newOrder: { type: String, required: true},
+    preparing: { type: String, required: true},
+    finished: { type: String, required: true},
 });
 
 module.exports = mongoose.model("CustomerPost", PostSchema);
